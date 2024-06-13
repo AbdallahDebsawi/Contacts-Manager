a8223b36129a21a0c0cc988aa7e1734a2883b996
AbdallahDebsawi <96738825+AbdallahDebsawi@users.noreply.github.com>
2024-06-13 22:35:41 +0300

    Lab 03

--- Contacts-Manager-Tests/UnitTest1.cs
+++ Contacts-Manager-Tests/UnitTest1.cs
@@ -7,6 +7,61 @@ namespace Contacts_Manager_Tests
 {
     public class UnitTest1
     {
-       
+        [Fact]
+        public void TestAddContact()
+        {
+            // Arrange
+            Program.contacts.Clear();
+
+            //Act
+            var updatedContacts = Program.AddContact("Abdallah Debsawi");
+
+            // Assert
+            Assert.Contains("Abdallah Debsawi", updatedContacts);
+        }
+
+        [Fact]
+        public void TestRemoveContact()
+        {
+            // Arrange
+            Program.contacts.Clear();
+
+            // Act
+            Program.AddContact("Abdallah Debsawi");
+            var updatedContacts = Program.RemoveContact("Abdallah Debsawi");
+
+            // Assert
+            Assert.DoesNotContain("Abdallah Debsawi", updatedContacts);
+        }
+
+        [Fact]
+        public void TestViewAllContacts()
+        {
+            // Arrange
+            Program.contacts.Clear();
+
+            // Act
+            Program.AddContact("Abdallah Debsawi");
+            Program.AddContact("dodo");
+            var contacts = Program.ViewAllContacts();
+
+            // Assert
+            Assert.Contains("Abdallah Debsawi", contacts);
+            Assert.Contains("dodo", contacts);
+        }
+
+        [Fact]
+        public void TestAddDuplicateContact()
+        {
+            // Arrange
+             Program.contacts.Clear();
+
+            // Act
+            Program.AddContact("Abdallah Debsawi");
+            var updatedContacts = Program.AddContact("Abdallah Debsawi");
+
+            // Assert
+            Assert.Single(updatedContacts);
+        }
     }
 }
--- Contacts-Manager/Program.cs
+++ Contacts-Manager/Program.cs
@@ -2,11 +2,49 @@
 {
     public class Program
     {
+       public static List<string> contacts = new List<string>();
 
         static void Main(string[] args)
         {
-            Console.WriteLine("hello, world!");
+            ContactsManager();
         }
-       
+        public static void ContactsManager()
+        {
+            AddContact("Abdallah Debsawi");
+            AddContact("soso");
+            AddContact("roro");
+            AddContact("nono");
+            AddContact("Ahmad Mohsen");
+            Console.WriteLine("the content list after adding new content");
+
+            Console.WriteLine(string.Join(", ", ViewAllContacts()));
+            RemoveContact("Ahmad Mohsen");
+            Console.WriteLine("the content list after removing the content");
+
+            Console.WriteLine(string.Join(", ", ViewAllContacts()));
+        }
+
+        public static List<string> AddContact(string contact)
+        {
+            if (!string.IsNullOrWhiteSpace(contact) && !contacts.Contains(contact))
+            {
+                contacts.Add(contact);
+            }
+            return new List<string>(contacts);
+        }
+
+        public static List<string> RemoveContact(string contact)
+        {
+            if (contacts.Contains(contact))
+            {
+                contacts.Remove(contact);
+            }
+            return new List<string>(contacts);
+        }
+
+        public static List<string> ViewAllContacts()
+        {
+            return new List<string>(contacts);
+        }
     }
 }
